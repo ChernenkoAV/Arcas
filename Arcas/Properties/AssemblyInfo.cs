62befae55419f1a48853fe7d15c3b7221ab3a487
ChernenkoAV <alexej.chernenko@gmail.com>
2021-12-02 23:41:54 +0300

    Чес.

--- Arcas/Properties/AssemblyInfo.cs
+++ Arcas/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //   Номер сборки
 //      Редакция
 //
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
